b5fad6fbb47ee593c54478b2c67d5f906a41f5cd
Eric Julianto <86970816+algonacci@users.noreply.github.com>
2023-09-29 08:10:09 +0700

    Update palette.py

--- palette.py
+++ palette.py
@@ -1,6 +1,22 @@
 import colorsys
 
 
+def adjust_brightness(hex_color, factor):
+    rgb = hex_to_rgb(hex_color)
+    h, l, s = colorsys.rgb_to_hls(*[x/255.0 for x in rgb])
+    l = max(min(l + factor, 1), 0)
+    rgb_adjusted = [int(x*255) for x in colorsys.hls_to_rgb(h, l, s)]
+    return rgb_to_hex(rgb_adjusted)
+
+
+def adjust_hue(hex_color, factor):
+    rgb = hex_to_rgb(hex_color)
+    h, l, s = colorsys.rgb_to_hls(*[x/255.0 for x in rgb])
+    h = (h + factor) % 1
+    rgb_adjusted = [int(x*255) for x in colorsys.hls_to_rgb(h, l, s)]
+    return rgb_to_hex(rgb_adjusted)
+
+
 def matching_gradient(hex_color):
     # Create a gradient by manipulating hue slightly
     return [adjust_hue(hex_color, i*0.05) for i in range(0, 10, 2)]
@@ -35,31 +51,27 @@ def rgb_to_hex(rgb):
     return '#{:02x}{:02x}{:02x}'.format(*rgb)
 
 
-def adjust_brightness(hex_color, factor):
+def hex_to_hls(hex_color):
+    # Convert HEX to HLS color space
     rgb = hex_to_rgb(hex_color)
-    h, l, s = colorsys.rgb_to_hls(*[x/255.0 for x in rgb])
-    l = max(min(l + factor, 1), 0)
-    rgb_adjusted = [int(x*255) for x in colorsys.hls_to_rgb(h, l, s)]
-    return rgb_to_hex(rgb_adjusted)
+    return colorsys.rgb_to_hls(*[x/255.0 for x in rgb])
 
 
-def adjust_hue(hex_color, factor):
-    rgb = hex_to_rgb(hex_color)
-    h, l, s = colorsys.rgb_to_hls(*[x/255.0 for x in rgb])
-    h = (h + factor) % 1
-    rgb_adjusted = [int(x*255) for x in colorsys.hls_to_rgb(h, l, s)]
-    return rgb_to_hex(rgb_adjusted)
+def hls_to_hex(h, l, s):
+    # Convert HLS to HEX color
+    rgb = colorsys.hls_to_rgb(h, l, s)
+    return rgb_to_hex([int(x*255) for x in rgb])
 
 
 def complementary_colors(hex_color):
-    """Generate a palette of complementary colors."""
+    # Generate a palette of complementary colors
     h, l, s = hex_to_hls(hex_color)
     complementary_hue = (h + 0.5) % 1.0
     return [hex_color, hls_to_hex(complementary_hue, l, s)]
 
 
 def triadic_colors(hex_color):
-    """Generate a triadic color palette."""
+    # Generate a triadic color palette
     h, l, s = hex_to_hls(hex_color)
     return [
         hex_color,
@@ -69,22 +81,10 @@ def triadic_colors(hex_color):
 
 
 def analogous_colors(hex_color):
-    """Generate an analogous color palette."""
+    # Generate an analogous color palette
     h, l, s = hex_to_hls(hex_color)
     return [
         hls_to_hex((h - 0.05) % 1.0, l, s),
         hex_color,
         hls_to_hex((h + 0.05) % 1.0, l, s)
     ]
-
-
-def hex_to_hls(hex_color):
-    """Convert HEX to HLS color space."""
-    rgb = hex_to_rgb(hex_color)
-    return colorsys.rgb_to_hls(*[x/255.0 for x in rgb])
-
-
-def hls_to_hex(h, l, s):
-    """Convert HLS to HEX color."""
-    rgb = colorsys.hls_to_rgb(h, l, s)
-    return rgb_to_hex([int(x*255) for x in rgb])
